418605f6ffb535f1b188da5b46307497ce6da1e0
54m1p <samipneupane.18@gmail.com>
2023-05-16 17:28:35 +0545

    practiced retrieval from local storage

--- bday-reminder/bdayCheck.js
+++ bday-reminder/bdayCheck.js
@@ -17,17 +17,26 @@ function addDetails(){
 }
 function bdayCheck(){
     let inpDate = new Date(document.getElementById("inpDate").value);
-     let list = localStorage.getItem("list") != null ? JSON.parse(localStorage.getItem("list")) : [];
+    let list = localStorage.getItem("list") != null ? JSON.parse(localStorage.getItem("list")) : [];
     let listDob, listName;
+    const dateFormat = {month: 'long', day: 'numeric'}
+    document.getElementById("result-text").innerHTML = " "; 
+    document.getElementById("result-head").innerHTML = " "; 
+    document.getElementById("no-result").innerHTML = " ";
+    let flag = false;
     for(let i = 0; i < list.length; i++){
         listName = list[i].name;
         listDob = new Date(list[i].dob);
         if(inpDate.getMonth() === listDob.getMonth() && inpDate.getDate() === listDob.getDate()){
-            const node = document.createElement("Div");
-            const text = document.createTextNode(listName + " has birthday on "+ listDob);
+            flag =true;
+            const node = document.createElement("li");
+            const text = document.createTextNode(listName);
             node.appendChild(text);
-            document.querySelector(".result").appendChild(node);
+            document.querySelector(".result-text").appendChild(node);
+            document.getElementById("result-head").innerHTML = "These people have birthdays on "+  inpDate.toLocaleString('en-US', dateFormat);
         }
     }
-
+    if(flag == false){
+        document.getElementById("no-result").innerHTML = "No one has birthday on "+  inpDate.toLocaleString('en-US', dateFormat);
+    }
 }
