1fcdddbdbf85ce4d2493e861f122ec6fb651377e
54m1p <samipneupane.18@gmail.com>
2023-05-22 18:05:35 +0545

    todo completed

--- toDo-list/todo.js
+++ toDo-list/todo.js
@@ -1,45 +1,53 @@
-var taskArr =[];
+var count = 1;
+var taskArr = []
 var addTask = () =>{
         let li = document.createElement("li");
+        li.id = "list-"+count;
         let node = document.createElement("input");
         node.type = 'checkbox';
         node.name = 'chkBox';
         node.className = "chkBox";
+        node.id = "checkbox-"+count;
+
         li.appendChild(node);
-        let inputText = document.getElementById("addTasks").value;
-        let tasks = document.createTextNode(inputText);
-        li.appendChild(tasks);
-        if(inputText === ""){
+        let label = document.createElement("label");
+        label.htmlFor = "checkbox-"+count;
+        label.innerHTML = document.getElementById("addTasks").value;
+        li.appendChild(label);  
+        if(label.innerHTML === ""){
                 alert("Cannot enter empty task");
         }
         else{
                 document.getElementById("myUl").appendChild(li);
+                taskArr.push(label.innerHTML);
         }
-        checkboxFn();
         document.getElementById("addTasks").value = "";
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("X");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-        // var close = document.getElementsByClassName("close");
-        // for(let i =0; i<close.length;i++){
-        //         close[i].onclick = closefn();
-        // }
-}
-var checkbox = document.getElementsByClassName("chkBox");
-var label = document.getElementsByTagName("li");
-var checkboxFn= ()=>{
-console.log("checkbox status", checkbox);
-console.log("label");
+        var del = document.createElement("a");
+        var txt = document.createTextNode("x");
+        del.className = "task-item close";
+        del.id = "close-"+count;
+        del.appendChild(txt);
+        label.appendChild(del);
+
+var checkbox = document.getElementById("checkbox-"+count);
+
 checkbox.addEventListener('change', (event)=>{
-        console.log("into the checked event");
         if(event.currentTarget.checked){
-                label.style.textDecoration = 'line-through';
-                console.log("checked");
+                label.style.textDecoration ='line-through';
         }
         else{
-                li.style.textDecoration = 'none';
+                label.style.textDecoration = 'none';
         }
 });
+count++;
+var list = document.getElementById("list-"+count);
+var close = document.getElementsByClassName("task-item");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function(e) {
+    var div = this.parentElement.parentElement;
+    div.remove();
+    //div.style.display = "none";
+    //checkBox[i].style.display = "none";
+  }
+}
 }
